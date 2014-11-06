93052f8282da03f293212c776f5dc8ea9e111b9e
Ethan Christian <ethan.e.christian@gmail.com>
2014-11-06 17:28:28 -0600

    Add flight

--- ardrone.py
+++ ardrone.py
@@ -2,11 +2,37 @@ import struct
 
 class ARDrone:
 	def __init__(self):
+		self.sequence_nbr = 1;
 		self.is_flying = False;
+		self.is_emergency_mode = False;
 
 	def set_navdata(self, data):
 		drone_info = dict()
-
+ 
 		navdata = struct.unpack_from("IIII", data, 0)
 
 		self.is_flying = navdata[1] & 1 == 1
+		self.is_emergency_mode = navdata[1] & 0x80000000
+
+	def flat_trims_command(self):
+		msg = "AT*FTRIM=%d\r" % self.sequence_nbr
+		self.sequence_nbr += 1
+		return msg
+
+	def take_off_command(self):
+		take_off_int = 0b10001010101000000001000000000
+		msg = "AT*REF=%i,%d\r" % (self.sequence_nbr, take_off_int)
+		self.sequence_nbr += 1
+		return msg
+
+	def land_command(self):
+		land_int = 0b10001010101000000000000000000
+		msg = "AT*REF=%i,%d\r" % (self.sequence_nbr, land_int)
+		self.sequence_nbr += 1
+		return msg
+
+	def emergency_stop (self):
+		stop_int = 0b10001010101000000000100000000
+		msg =  "AT*REF=%i,%d\r" % (self.sequence_nbr, stop_int)
+		self.sequence_nbr += 1
+		return msg
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 import struct
 import ardrone
+import time
 
 # 5556 send
 # 5554 receive navdata
@@ -14,7 +15,7 @@ DRONE_NAV_PORT = 5554
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.connect((DRONE_IP, DRONE_PORT))
+
 print 'Connected to drone'
 
 nav_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,6 +26,7 @@ print 'Server socket set up'
 
 def exit(signal, frame):
 	#sock.shutdown()
+	sock.sendto(drone.emergency_stop(),(DRONE_IP, DRONE_PORT))
 	sock.close()
 
 	#serversocket.shutdown()
@@ -39,9 +41,20 @@ drone = ardrone.ARDrone()
 
 nav_sock.sendto("\x01\x00\x00\x00", (DRONE_IP, DRONE_NAV_PORT))
 
+sock.sendto(drone.flat_trims_command(),(DRONE_IP,DRONE_PORT)) # Levels the trim for flight
+time.sleep(.5)
+
+
 while True:
 	data = nav_sock.recv(65535)
 
 	drone.set_navdata(data)
+
 	if not drone.is_flying:
 		print "Not flying"
+		#print drone.take_off_command()
+		sock.sendto(drone.take_off_command(),(DRONE_IP, DRONE_PORT))
+	else:
+		print "Flying"
+		sock.sendto(drone.take_off_command(),(DRONE_IP, DRONE_PORT))
+	time.sleep(.03)
