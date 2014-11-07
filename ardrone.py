1c6b98d8e1203373d9d5178499c92f75c107e4f8
Ethan Christian <ethan.e.christian@gmail.com>
2014-11-07 11:15:15 -0600

    Move sockets to ardrone

--- ardrone.py
+++ ardrone.py
@@ -1,38 +1,62 @@
 import struct
+import socket
+
+# 5556 send
+# 5554 receive navdata
+CTRL_PORT = 5556
+STATUS_PORT = 5554
 
 class ARDrone:
-	def __init__(self):
+	def __init__(self,IP_ADDRESS="192.168.1.1"):
 		self.sequence_nbr = 1;
+		self.ip_address = IP_ADDRESS;
+		self.ctrl_socket = socket.socket()
+		self.status_socket = socket.socket()
 		self.is_flying = False;
 		self.is_emergency_mode = False;
 
-	def set_navdata(self, data):
+
+	def connect(self):
+		self.ctrl_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.status_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.status_socket.bind(('', STATUS_PORT))
+		self.status_socket.sendto("\x01\x00\x00\x00", (self.ip_address, STATUS_PORT))
+
+	def set_navdata(self):
 		drone_info = dict()
- 
+ 		data = self.status_socket.recv(65535)
 		navdata = struct.unpack_from("IIII", data, 0)
 
 		self.is_flying = navdata[1] & 1 == 1
 		self.is_emergency_mode = navdata[1] & 0x80000000 == 1
 
-	def flat_trims_command(self):
+	def flat_trims(self):    # Levels the trim for flight !!! Must be on the ground, level!!!
 		msg = "AT*FTRIM=%d\r" % self.sequence_nbr
 		self.sequence_nbr += 1
-		return msg
+		self.send_ctrl_cmd(msg)
 
-	def take_off_command(self):
+	def take_off(self):
 		take_off_int = 0b10001010101000000001000000000
 		msg = "AT*REF=%i,%d\r" % (self.sequence_nbr, take_off_int)
 		self.sequence_nbr += 1
-		return msg
+		self.send_ctrl_cmd(msg)
 
-	def land_command(self):
+	def land(self):
 		land_int = 0b10001010101000000000000000000
 		msg = "AT*REF=%i,%d\r" % (self.sequence_nbr, land_int)
 		self.sequence_nbr += 1
-		return msg
+		self.send_ctrl_cmd(msg)
 
-	def emergency_stop (self):
+	def emergency_stop(self):
 		stop_int = 0b10001010101000000000100000000
 		msg =  "AT*REF=%i,%d\r" % (self.sequence_nbr, stop_int)
 		self.sequence_nbr += 1
-		return msg
+		self.send_ctrl_cmd(msg)
+
+	def send_ctrl_cmd(self,msg):
+		self.ctrl_socket.sendto(msg, (self.ip_address,CTRL_PORT))
+
+	def __exit__(self,type,value,traceback):
+		self.ctrl_socket.close()
+		self.status_socket.close()
+		
--- main.py
+++ main.py
@@ -1,60 +1,16 @@
-import socket
-import signal
-import sys
 
-import struct
 import ardrone
 import time
 
-# 5556 send
-# 5554 receive navdata
-
-DRONE_IP = "192.168.1.1"
-DRONE_PORT = 5556
-DRONE_NAV_PORT = 5554
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-print 'Connected to drone'
-
-nav_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#nav_sock.setblocking(0)
-nav_sock.bind(('', DRONE_NAV_PORT))
-print 'Server socket set up'
-
-
-def exit(signal, frame):
-	#sock.shutdown()
-	sock.sendto(drone.emergency_stop(),(DRONE_IP, DRONE_PORT))
-	sock.close()
-
-	#serversocket.shutdown()
-	nav_sock.close()
-	print "Cleaned up"
-	sys.exit(0)
-
-signal.signal(signal.SIGINT, exit)
-
 drone = ardrone.ARDrone()
-	
-
-nav_sock.sendto("\x01\x00\x00\x00", (DRONE_IP, DRONE_NAV_PORT))
-
-sock.sendto(drone.flat_trims_command(),(DRONE_IP,DRONE_PORT)) # Levels the trim for flight
 time.sleep(.5)
 
+drone.flat_trims()
 
 while True:
-	data = nav_sock.recv(65535)
-
-	drone.set_navdata(data)
+	drone.set_navdata
 
 	if not drone.is_flying:
 		print "Not flying"
-		#print drone.take_off_command()
-		sock.sendto(drone.take_off_command(),(DRONE_IP, DRONE_PORT))
-	else:
-		print "Flying"
-		sock.sendto(drone.take_off_command(),(DRONE_IP, DRONE_PORT))
+		drone.take_off()
 	time.sleep(.03)
