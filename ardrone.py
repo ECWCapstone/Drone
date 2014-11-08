948b74c88cb504b3892bfba47adb8491a07e45e1
Ethan Christian <ethan.e.christian@gmail.com>
2014-11-08 16:13:04 -0600

    Add multithraeded queue, todo: nicer exit

--- ardrone.py
+++ ardrone.py
@@ -1,6 +1,9 @@
 import struct
 import socket
 
+import multiprocessing as mp
+import time
+
 # 5556 send
 # 5554 receive navdata
 CTRL_PORT = 5556
@@ -8,12 +11,15 @@ STATUS_PORT = 5554
 
 class ARDrone:
 	def __init__(self,IP_ADDRESS="192.168.1.1"):
-		self.sequence_nbr = 1;
+
+		self.cmd_queue = mp.Queue()
+		self.processes = []
+
 		self.ip_address = IP_ADDRESS;
 		self.ctrl_socket = socket.socket()
 		self.status_socket = socket.socket()
-		self.is_flying = False;
-		self.is_emergency_mode = False;
+		self.is_flying = mp.Value('b', False);
+		self.is_emergency_mode = mp.Value('b', False);
 
 
 	def connect(self):
@@ -22,41 +28,77 @@ class ARDrone:
 		self.status_socket.bind(('', STATUS_PORT))
 		self.status_socket.sendto("\x01\x00\x00\x00", (self.ip_address, STATUS_PORT))
 
-	def set_navdata(self):
+	def run(self):
+		p = mp.Process(target=self.send_from_queue, args=(self.cmd_queue,))  # Queue dispatch thread 
+		p2 = mp.Process(target=self.update_navdata, args=(self.is_flying,self.is_emergency_mode))  # Nav data listener thread
+
+		self.processes.append(p)
+		self.processes.append(p2)
+		p.start()
+		p2.start()
+
+	def __set_navdata(self,flying,emergency_mode):
 		drone_info = dict()
  		data = self.status_socket.recv(65535)
 		navdata = struct.unpack_from("IIII", data, 0)
 
-		self.is_flying = navdata[1] & 1 == 1
-		self.is_emergency_mode = navdata[1] & 0x80000000 == 1
+		flying = navdata[1] & 1 == 1
+		emergency_mode = navdata[1] & 0x80000000 == 1
 
 	def flat_trims(self):    # Levels the trim for flight !!! Must be on the ground, level!!!
-		msg = "AT*FTRIM=%d\r" % self.sequence_nbr
-		self.sequence_nbr += 1
-		self.send_ctrl_cmd(msg)
+		cmd_tup = ("FTRIM", "")
+		self.cmd_queue.put(cmd_tup)
 
 	def take_off(self):
 		take_off_int = 0b10001010101000000001000000000
-		msg = "AT*REF=%i,%d\r" % (self.sequence_nbr, take_off_int)
-		self.sequence_nbr += 1
-		self.send_ctrl_cmd(msg)
+		cmd_tup = ("REF", (",%d" % take_off_int))
+		self.cmd_queue.put(cmd_tup)
 
 	def land(self):
 		land_int = 0b10001010101000000000000000000
-		msg = "AT*REF=%i,%d\r" % (self.sequence_nbr, land_int)
-		self.sequence_nbr += 1
-		self.send_ctrl_cmd(msg)
+		cmd_tup = ("REF", (",%d" % land_int))
+		self.cmd_queue.put(cmd_tup)
 
 	def emergency_stop(self):
 		stop_int = 0b10001010101000000000100000000
-		msg =  "AT*REF=%i,%d\r" % (self.sequence_nbr, stop_int)
-		self.sequence_nbr += 1
-		self.send_ctrl_cmd(msg)
+		cmd_tup = ("REF", (",%d" % stop_int))
+		self.cmd_queue.put(cmd_tup)
+
+	def hover(self):
+		cmd_tup = ("PCMD", ",0,0,0,0,0")
+		self.cmd_queue.put(cmd_tup)
 
 	def send_ctrl_cmd(self,msg):
 		self.ctrl_socket.sendto(msg, (self.ip_address,CTRL_PORT))
 
+	def send_from_queue(self,queue):
+		sequence_nbr = 1
+		no_op_cmd = ("PCMD", ",0,0,0,0,0")
+		while True:
+			if queue.empty():
+				msg = "AT*%s=%i%s\r" % (no_op_cmd[0],sequence_nbr,no_op_cmd[1])
+				sequence_nbr += 1
+				self.send_ctrl_cmd(msg)
+				time.sleep(.5)
+
+			else:
+				cmd_tup = queue.get()
+				msg = "AT*%s=%i%s\r" % (cmd_tup[0],sequence_nbr,cmd_tup[1])
+				sequence_nbr += 1
+				self.send_ctrl_cmd(msg)
+
+			
+
+	def update_navdata(self,flying,emergency_mode):
+		while True:
+			self.__set_navdata(flying,emergency_mode)
+			time.sleep(.25)
+
 	def __exit__(self,type,value,traceback):
+
+		(thrd.cancel for thrd in self.threads)
+		
+
 		self.ctrl_socket.close()
 		self.status_socket.close()
 		
--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-
 import ardrone
+import sys
+import signal
 import time
 
-drone = ardrone.ARDrone()
-time.sleep(.5)
+import sys, tty, termios
 
+
+
+##############################################################################################################
+### WTF python, Really?
+##############################################################################################################
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+
+drone = ardrone.ARDrone()
+drone.connect()
+drone.run()
 drone.flat_trims()
 
 while True:
-	drone.set_navdata
 
-	if not drone.is_flying:
-		print "Not flying"
+	
+	
+	cmd = getch()
+
+	if ord(cmd) == 3:     # looking for ctrl + c
+		sys.exit(0)
+
+	
+	if cmd == 'l' or cmd == 'L':
+		drone.land()
+	elif cmd == 't':
 		drone.take_off()
-	time.sleep(.03)
+	else:
+		pass
+
+
+	#drone.emergency_stop()
+	#time.sleep(.03) # dont overflow the queue
