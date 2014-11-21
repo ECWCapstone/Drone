2b5ec7d71386da3e99578c024b087d085b6f7c91
Ethan Christian <ethan.e.christian@gmail.com>
2014-11-21 11:52:27 -0600

    Add error catching

--- main.py
+++ main.py
@@ -29,40 +29,41 @@ print "Drone ready"
 
 while True:
 
-	
-	cmd = getch()
+	try: 
+		cmd = getch()
 
-	if ord(cmd) == 3:# looking for ctrl + c
-		drone.disconect()
-		sys.exit(0)
+		if ord(cmd) == 3:# looking for ctrl + c
+			drone.disconect()
+			sys.exit(0)
 
-	
-	if cmd == 'g':
-		drone.land()
-	elif cmd == 't':
-		print "Taking off"
-		drone.take_off()
-	elif cmd == 'a':
-		drone.left()
-	elif cmd == 'd':
-		drone.right()
-	elif cmd =='w':
-		drone.forward()
-	elif cmd == 's':
-		drone.backward()
-	elif cmd == 'i':
-		drone.up()
-	elif cmd == 'k':
-		drone.down()
-	elif cmd == 'j':
-		drone.rotate_left()
-	elif cmd == 'l':
-		drone.rotate_right()
-	elif cmd == 'e':
-		drone.emergency_stop()
-	else:
-		pass
-
-
+		
+		if cmd == 'g':
+			drone.land()
+		elif cmd == 't':
+			print "Taking off"
+			drone.take_off()
+		elif cmd == 'a':
+			drone.left()
+		elif cmd == 'd':
+			drone.right()
+		elif cmd =='w':
+			drone.forward()
+		elif cmd == 's':
+			drone.backward()
+		elif cmd == 'i':
+			drone.up()
+		elif cmd == 'k':
+			drone.down()
+		elif cmd == 'j':
+			drone.rotate_left()
+		elif cmd == 'l':
+			drone.rotate_right()
+		elif cmd == 'e':
+			drone.emergency_stop()
+		else:
+			pass
+	except Exception as e:
+		print type(e)           
+		print "It was Will's fault"
 	#drone.emergency_stop()
 	#time.sleep(.03) # dont overflow the queue
